7d6904edb471f3e5bf5523e0b27846cdc2a70092
Alexandr <skv43r@gmail.com>
2024-12-17 18:06:49 +0300

    feat(Publish Subscribe): Sending messages to many consumers at once

---
+++ Publish Subscribe/emit_log.py
@@ -0,0 +1,16 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message)
+
+print(f" [x] Sent {message}")
+connection.close()
